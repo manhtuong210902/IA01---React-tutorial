06816fa18713aa035f08490c450ad2a15e26c769
20120619 <mtuong669@gmail.com>
2023-10-15 18:09:01 +0700

    add info

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import "./App.css";
 import Game from "./components/Game";
 
 function App() {
-    return <Game />;
+    return (
+        <div>
+            <ul>
+                <li>Student ID: 20120619</li>
+                <li>Full Name: Nguyen Manh Tuong</li>
+            </ul>
+            <Game />
+        </div>
+    );
 }
 
 export default App;
